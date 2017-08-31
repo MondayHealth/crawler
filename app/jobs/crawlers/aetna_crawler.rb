dc58f65b2f42f67fe5f4a89d2135920510576c0d
Benjamin Jackson <bhjackson@gmail.com>
2017-08-31 17:20:47 -0400

    The Aetna crawler now ensures the web browser is quit after running even if an exception is raised.

--- app/jobs/crawlers/aetna_crawler.rb
+++ app/jobs/crawlers/aetna_crawler.rb
@@ -17,39 +17,44 @@ module Jobs
         page_source = nil
         Headless.ly do
           begin
-          @driver = Selenium::WebDriver.for :firefox
-          @driver.navigate.to url
-          @wait.until do
+            @driver = Selenium::WebDriver.for :firefox
+            @driver.navigate.to url
+            @wait.until do
+              begin
+                # Did we find any results?
+                @driver.find_element(id: "pageNumbers")
+                true
+              rescue Selenium::WebDriver::Error::ServerError => e
+                unless e.message =~ /404/
+                  raise e
+                end
+              rescue Selenium::WebDriver::Error::NoSuchElementError => e
+                # Are we on an empty results page?
+                @driver.find_element(id: "noResultsSection")
+                true
+              end
+            end
+
             begin
               # Did we find any results?
               @driver.find_element(id: "pageNumbers")
-              true
-            rescue Selenium::WebDriver::Error::ServerError => e
-              unless e.message =~ /404/
-                raise e
-              end
+
+              # we need to strip out tabs and newlines here since they mess with ssdb-rb's GET method
+              # might as well get rid of extra space characters while we're at it
+              page_source = @driver.page_source.gsub("\n", " ").gsub("\t", " ").gsub(/\s+/, " ")
+              @ssdb.set(url, page_source)
+
+              STDOUT.puts("Enqueueing AetnaScraper with [#{plan_id}, #{url}]")
+              Resque.push('scraper_aetna', :class => 'Jobs::Scrapers::AetnaScraper', :args => [plan_id, url])
             rescue Selenium::WebDriver::Error::NoSuchElementError => e
-              # Are we on an empty results page?
-              @driver.find_element(id: "noResultsSection")
-              true
+              STDOUT.puts("No results found for [#{plan_id}, #{url}]")
             end
+            @driver.quit
+          rescue Exception => e
+            # Make sure we quit the browser even if we run into an exception we didn't anticipate
+            @driver.quit
+            raise e
           end
-
-          begin
-            # Did we find any results?
-            @driver.find_element(id: "pageNumbers")
-
-            # we need to strip out tabs and newlines here since they mess with ssdb-rb's GET method
-            # might as well get rid of extra space characters while we're at it
-            page_source = @driver.page_source.gsub("\n", " ").gsub("\t", " ").gsub(/\s+/, " ")
-            @ssdb.set(url, page_source)
-
-            STDOUT.puts("Enqueueing AetnaScraper with [#{plan_id}, #{url}]")
-            Resque.push('scraper_aetna', :class => 'Jobs::Scrapers::AetnaScraper', :args => [plan_id, url])
-          rescue Selenium::WebDriver::Error::NoSuchElementError => e
-            STDOUT.puts("No results found for [#{plan_id}, #{url}]")
-          end
-          @driver.quit
         end
       end
     end
