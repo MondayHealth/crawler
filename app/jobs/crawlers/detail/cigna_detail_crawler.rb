040cc932441cc434bd4464ae84c592b4a2330bed
Benjamin Jackson <bhjackson@gmail.com>
2017-10-25 17:24:12 -0400

    Removed debug log from CignaDetailCrawler.

--- app/jobs/crawlers/detail/cigna_detail_crawler.rb
+++ app/jobs/crawlers/detail/cigna_detail_crawler.rb
@@ -26,7 +26,6 @@ module Jobs
             headers = {}
             headers["cookie"] = options["cookie"]
             page_source = RestClient.get(url, headers)
-            puts page_source
           end
 
           self.ssdb.set(cache_key, sanitize_for_ssdb(page_source))
