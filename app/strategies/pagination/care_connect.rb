ac3d259a391f8f5d6f96b72f064cc47fc4d00b6a
Benjamin Jackson <bhjackson@gmail.com>
2017-10-25 15:01:45 -0400

    Swapped out timed sleeps in CareConnect crawler for additional Selenium wait directives, which fix the 404s coming from the Selenium server. Not sure what's causing these, but wondering if it might have something to do with the server specs since RAM on the box is much lower than on local dev laptop.

--- app/strategies/pagination/care_connect.rb
+++ app/strategies/pagination/care_connect.rb
@@ -9,7 +9,7 @@ module Monday
         @job_class = 'Jobs::Crawlers::CareConnectCrawler'
 
         def enqueue_all plan
-          @wait = Selenium::WebDriver::Wait.new(timeout: 20) # seconds
+          @wait = Selenium::WebDriver::Wait.new(timeout: 20, ignore: [Selenium::WebDriver::Error::NoSuchElementError, Selenium::WebDriver::Error::ServerError]) # seconds
           Headless.ly do
             @driver = Selenium::WebDriver.for :firefox
             begin
@@ -17,8 +17,9 @@ module Monday
               @driver.navigate.to url
               plan_link = nil
               @wait.until do
-                sleep 1
                 plan_link = @driver.find_element(xpath: "//a[contains(text(), 'CareConnect')]")
+              end
+              @wait.until do
                 plan_link.enabled? && plan_link.displayed?
               end
               plan_link.click
@@ -29,8 +30,9 @@ module Monday
 
               provider_link = nil
               @wait.until do
-                sleep 1
                 provider_link = @driver.find_element(xpath: "//a[contains(text(), 'Behavioral Health')]")
+              end
+              @wait.until do
                 provider_link.enabled? && provider_link.displayed?
               end
               provider_link.click
