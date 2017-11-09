59f18d545ab6ff36d4c04c374828ae0ecc5ed4f4
Benjamin Jackson <bhjackson@gmail.com>
2017-11-08 20:05:36 -0500

    Removed debug code from rescue block.

--- app/jobs/crawlers/psychology_today_crawler.rb
+++ app/jobs/crawlers/psychology_today_crawler.rb
@@ -77,7 +77,6 @@ module Jobs
             @driver.quit
           rescue Exception => e
             # Make sure we quit the browser even if we run into an exception we didn't anticipate
-            STDOUT.puts @driver.page_source
             @driver.quit
             raise e  
           end
