d0cd36e1b4bc2cef738f5897e5f744dbd71037dc
Benjamin Jackson <bhjackson@gmail.com>
2017-11-10 17:20:19 -0500

    Added pagination strategy and crawler for Empire Blue Cross Blue Shield.

---
+++ app/strategies/pagination/empire.rb
@@ -0,0 +1,96 @@
+require_relative 'base'
+
+module Monday
+  module Strategies
+    module Pagination
+      class Empire < Base
+
+        @queue_name = 'crawler_empire'
+        @job_class = 'Jobs::Crawlers::EmpireCrawler'
+
+        DISTANCE = 10
+        PER_PAGE = 100
+        BASE_QUERY_STRING = '&alphaPrefix=&bcbsaProductId/results/acceptingNewPatients=false&alphaPrefix=&boardCertified=&hasExtendedHours=false&gender=&isEligiblePCP=false&maxLatitude=&maxLongitude=&minLatitude=&minLongitude=&name=&patientAgeRestriction=&patientGenderRestriction=&providerCategory=P&qualityRecognitions=&searchType=advanced&sort=DEFAULT'
+
+        # Each provider type maps to an optional list of specialty IDs, 
+        # e.g. "Physicians specializing in Anxiety"
+        PROVIDER_TYPES_AND_SPECIALTY_IDS = {
+          "CNSLR" => [],
+          "PHYSC" => ["8183575", "9395761", "8278232", "2791940", "9783466", "8433601"],
+          "PHYAST" => ["NC75052", "NC74907", "LC70919"]
+        }
+
+        def enqueue_all plan
+          PROVIDER_TYPES_AND_SPECIALTY_IDS.each_pair do |provider_type, specialty_ids|
+            url = plan.url + BASE_QUERY_STRING
+            url += "&productCode=#{plan.original_code}"
+            url += "&size=#{PER_PAGE}"
+            url += "&radius=#{DISTANCE}"
+            url += "&providerType=#{provider_type}"
+            specialty_ids.each do |specialty_id|
+              url += "&specialties=#{specialty_id}"
+            end
+            url += "&page=1"
+            record_limit = fetch_record_limit(url)
+
+            current_url = url
+
+            while !self.hit_record_limit? current_url, record_limit
+              yield current_url
+              current_url = self.next_page(current_url)
+            end
+          end
+        end
+
+        def next_page url
+          url.sub(/page=([0-9]*)/) { "page=#{Regexp.last_match[1].to_i + 1}" }
+        end
+
+        def hit_record_limit? url, limit
+          page = 0
+          url.scan(/page=([0-9]*)/) do 
+            page = Regexp.last_match[1].to_i
+          end
+          page * PER_PAGE > limit + 1
+        end
+
+        def fetch_record_limit url
+          puts "Fetching record limit from #{url}"
+          @wait = Selenium::WebDriver::Wait.new(timeout: 60) # seconds
+          Headless.ly do
+            results_count_string = nil
+
+            caps = Selenium::WebDriver::Remote::Capabilities.firefox
+            caps['acceptInsecureCerts'] = true
+
+            @driver = Selenium::WebDriver.for :firefox, desired_capabilities: caps
+            begin
+              @driver.navigate.to url
+              @wait.until do
+                begin
+                  results_count = @driver.find_element(css: "div[data-test='results-count-greater-than-one']")
+                rescue Selenium::WebDriver::Error::NoSuchElementError => e
+                  # Are we on an empty results page?
+                  @driver.find_element(id: "results-container")
+                  true
+                end
+
+                results_count_string = results_count.text
+                true
+              end
+              @driver.quit
+            rescue Exception => e
+              # Make sure we quit the browser even if we run into an exception we didn't anticipate
+              @driver.quit
+              raise e
+            end
+
+            results_count_match = results_count_string.match(/\s+of\s+([0-9,]+)\s+results/)
+            return results_count_match[1].gsub(',', '').to_i
+          end
+        end
+
+      end
+    end
+  end
+end
