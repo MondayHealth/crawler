e6fb84c62cd61628277b1bed7ca35b4f31332a54
Benjamin Jackson <bhjackson@gmail.com>
2017-11-10 17:16:46 -0500

    Refactored refresh tasks for payors into background jobs for each plan.

---
+++ app/jobs/refresh.rb
@@ -0,0 +1,14 @@
+module Jobs
+  class Refresh
+    @queue = :crawler_refresh_payor
+
+    def self.perform plan_id
+      plan = Plan.find(plan_id)
+      strategy = plan.pagination_strategy
+      strategy.enqueue_all(plan) do |url, options={}|
+        STDOUT.puts("Enqueueing #{strategy.class.job_class} with [#{plan.id}, #{url}, #{options.inspect}]")
+        Resque.push(strategy.class.queue_name, :class => strategy.class.job_class, :args => [plan.id, url, options])
+      end
+    end
+  end
+end
