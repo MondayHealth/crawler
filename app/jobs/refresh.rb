858cb66c0e032a7f55f1cb238500d1070bbe3bd5
Benjamin Jackson <bhjackson@gmail.com>
2017-11-11 11:16:10 -0500

    Fixed runtime error caused by error in naming job class.

--- app/jobs/refresh.rb
+++ app/jobs/refresh.rb
@@ -1,13 +1,15 @@
-module Jobs
-  class Refresh
-    @queue = :crawler_refresh_payor
+module Monday
+  module Jobs
+    class Refresh
+      @queue = :crawler_refresh_payor
 
-    def self.perform plan_id
-      plan = Plan.find(plan_id)
-      strategy = plan.pagination_strategy
-      strategy.enqueue_all(plan) do |url, options={}|
-        STDOUT.puts("Enqueueing #{strategy.class.job_class} with [#{plan.id}, #{url}, #{options.inspect}]")
-        Resque.push(strategy.class.queue_name, :class => strategy.class.job_class, :args => [plan.id, url, options])
+      def self.perform plan_id
+        plan = Plan.find(plan_id)
+        strategy = plan.pagination_strategy
+        strategy.enqueue_all(plan) do |url, options={}|
+          STDOUT.puts("Enqueueing #{strategy.class.job_class} with [#{plan.id}, #{url}, #{options.inspect}]")
+          Resque.push(strategy.class.queue_name, :class => strategy.class.job_class, :args => [plan.id, url, options])
+        end
       end
     end
   end
